f718ffe87a63b7870f7007efd3866ed7d7de243d
penny <penny@pennydeMacBook-Air.local>
2024-11-04 12:37:54 +0800

    mac adds two js file in frontend for decoupling the rtc js

---
+++ frontend/src/components/rtcControl.js
@@ -0,0 +1,7 @@
+rootdir and refresh when build new connection
+lscurrent
+enterdir(加入返回上一级)
+downloadfile
+previewfile
+
+考虑异步
---
+++ frontend/src/components/rtcData.js
@@ -0,0 +1,3 @@
+ filetransfer-》bigfile transfer （分片when file exceed n MB）
+ preview file
+ upload add download side
